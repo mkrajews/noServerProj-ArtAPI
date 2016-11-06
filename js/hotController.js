0f525517147f955ae4257f0392a74eec8877dec5
mkrajews <mimikrajewski@Jakes-MBP-2.home>
2016-11-06 03:38:48 -0700

    update again

--- js/hotController.js
+++ js/hotController.js
@@ -1,6 +1,5 @@
 angular.module('artApp').controller('hotController', function($scope, mainSvc, $rootScope) {
     $rootScope.hotpics = [];
-
     //get request for art in here, then set the response to a $scope variable, and then display the info without ng-repeat
     $scope.getArt = function() {
         // console.log("getting art");
