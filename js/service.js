90b597bcac926fd56ccbfca6225040b09b134dfb
mkrajews <mimikrajewski@yahoo.com>
2017-02-13 19:19:28 -0700

    styling changes

--- js/service.js
+++ js/service.js
@@ -3,14 +3,14 @@ angular.module("artApp").service("mainSvc", function($http) {
     var baseUrl = "http://api.harvardartmuseums.org";
 
     this.getArtInfo = function() {
-        console.log("apple Pies");
+        // console.log("getArtInfo hit");
         return $http({
 
             method: "GET",
             url: "http://api.harvardartmuseums.org/object?apikey=bd9c2c20-a116-11e6-ac56-6f22b9ea04b3&size=1&sort=random&hasimage=1&classification=Paintings"
         }).then(function(response) {
             console.log(response);
-            console.log("hi");
+            // console.log("hit");
             return response.data;
         });
     };
